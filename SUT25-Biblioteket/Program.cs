b8930b6345261139634e9e7e4e65a93200adef7a
tonilingardsson <toni.lingardsson@gmail.com>
2025-10-09 11:40:20 +0200

    Updated Program class with book create and display functions

--- SUT25-Biblioteket/Program.cs
+++ SUT25-Biblioteket/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System;
+
+namespace LibrarySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating one book
+            Book book1 = new Book();
+            book1.Id = 1;
+            book1.Title = "Harry Potter";
+            book1.TotalCopies = 3;
+            book1.BorrowedCopies = 0;
+
+            // Creating second book
+            Book book2 = new Book();
+            book2.Id = 2;
+            book2.Title = "Batman";
+            book2.TotalCopies = 8;
+            book2.BorrowedCopies = 0;
+
+            // Creating third book
+            Book book3 = new Book();
+            book3.Id = 3;
+            book3.Title = "Superman";
+            book3.TotalCopies = 3;
+            book3.BorrowedCopies = 0;
+
+            book1.DisplayBook();
+            book2.DisplayBook();
+            book3.DisplayBook();
+        }
+    }    
+}
